b7a473bc13b3a71d5a0360fa1d6fcb2500432845
pranay <pranayharishchandra@gmail.com>
2024-02-16 13:09:18 +0530

    notes - 5 - env variables

---
+++ full-stack-dev-2023-node-6/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const server = express();
+const productRouter = require('./routes/product')
+const userRouter = require('./routes/user')
+console.log('env',process.env.DB_PASSWORD)
+//bodyParser
+server.use(express.json());
+server.use(morgan('default'));
+server.use(express.static(process.env.PUBLIC_DIR));
+
+// routes
+server.use('/products',productRouter.router);
+server.use('/users',userRouter.router);
+
+server.listen(process.env.PORT, () => {
+  console.log('server started');
+});
--- full-stack-dev-2023-node-6/routes/product.js
+++ full-stack-dev-2023-node-6/routes/product.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const productController = require('../controller/product');
 
-// we have already created server in index.js running on port 3000
-// we can't make 2 servers... so we will make 
-
 const router = express.Router();
 
 router
-  .get('/', productController.getAllProducts)
   .post('/', productController.createProduct)
+  .get('/', productController.getAllProducts)
   .get('/:id', productController.getProduct)
   .put('/:id', productController.replaceProduct)
   .patch('/:id', productController.updateProduct)
   .delete('/:id', productController.deleteProduct);
 
 exports.router = router;  
-
-// --- index.js --- 
-// const productRouter = require('./routes/product')
-// server.use('/products',productRouter.router);
