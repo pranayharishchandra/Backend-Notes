96b726d3f6bcf278191b1fa85dcf248ff03737d5
pranay <pranayharishchandra@gmail.com>
2024-02-16 09:26:45 +0530

    notes 3 4 5t

---
+++ full-stack-dev-2023-node-5/index.js
@@ -0,0 +1,18 @@
+const express       = require('express');
+const morgan        = require('morgan');
+const server        = express();
+const productRouter = require('./routes/product')
+const userRouter    = require('./routes/user')
+
+//bodyParser
+server.use(express.json());
+server.use(morgan('default'));
+server.use(express.static('public'));
+
+// router
+server.use('/products',productRouter.router);
+server.use('/users',userRouter.router);
+
+server.listen(8080, () => {
+  console.log('server started');
+});
